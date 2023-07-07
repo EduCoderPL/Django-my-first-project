37a9cccec5518805c17bb2be120f1aa8570287cb
Arkadiusz Mazurek <66950904+AMFeniksPL@users.noreply.github.com>
2023-07-07 13:08:59 +0200

    Added messages management

--- phoenix/base/urls.py
+++ phoenix/base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('login/', views.login_page, name="login"),
     path('logout/', views.logout_user, name="logout"),
-    path('register/', views.register_page, name="register")
+    path('register/', views.register_page, name="register"),
+    path('delete-message/<str:pk>/', views.delete_message, name="delete-message"),
 
 ]
--- phoenix/base/views.py
+++ phoenix/base/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -84,7 +84,18 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == "POST":
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+
+
+    context = {"room": room, 'room_messages': room_messages, "participants": participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url = "login")
@@ -115,6 +126,7 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url = "login")
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     if request.user != room.host:
@@ -123,3 +135,13 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, "base/delete.html", {"obj": room})
+
+@login_required(login_url = "login")
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse("A co Ty tutaj chcesz?!")
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, "base/delete.html", {"obj": message})
